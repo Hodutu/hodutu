20d7a8368aea0ddd4a705fc8aea025b72d682ce3
Michal Budzynski <michal@virtualdesign.pl>
2014-03-20 10:12:00 +0100

    [HODUTU] - Autocomplete works

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,3 +1,17 @@
 var listDb = require('./js/tvshow-list');
 
-alert('elo!');
+var input = document.getElementById('search');
+var autocomplete = document.getElementById('autocomplete');
+
+input.addEventListener('input', function() {
+  var text = input.value;
+  autocomplete.innerHTML = '';
+  if (text.length > 2) {
+    listDb.find({ title: new RegExp(text, 'ig') }, function (err, docs) {
+      //console.log('E', docs);
+      docs.forEach(function(element) {
+        autocomplete.innerHTML += "<li>" + element.title + "</li>";
+      });
+    });
+  }
+});
