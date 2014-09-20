f0f71c6ea163a74130699a657ac0e4ec0008195a
Michał Budzyński <michal@virtualdesign.pl>
2014-09-20 01:01:00 +0100

    [HODUTU] - :kissing_cat: j2nedb generator iterates thu tv shows

--- tools/json-to-nedb.js
+++ tools/json-to-nedb.js
@@ -1,6 +1,8 @@
 var Nedb = require('nedb');
 var userJsonData = require(__dirname + '/../src/trash-data/data.js');
 
-//var userList = new Nedb({ filename: 'src/data/user_data.db', autoload: true });
+var userList = new Nedb({ filename: 'src/data/user_data.db', autoload: true });
 
-console.log(userJsonData);
+userJsonData.forEach(function(show){
+  console.log(show.name);
+})
