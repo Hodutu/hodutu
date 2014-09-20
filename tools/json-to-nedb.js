72b6009c47541a407d279a04c636de75f4209f1c
Michał Budzyński <michal@virtualdesign.pl>
2014-09-20 01:01:00 +0100

    [TV] - :scream: j2nedb sample

---
+++ tools/json-to-nedb.js
@@ -0,0 +1,6 @@
+var Nedb = require('nedb');
+var userJsonData = require('src/trash-data/data.js');
+
+var userList = new Nedb({ filename: 'src/data/user_data.db', autoload: true });
+
+console.log(userJsonData);
