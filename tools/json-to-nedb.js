03188f127e01656798a2febec3deaf158f3ff243
Michał Budzyński <michal@virtualdesign.pl>
2014-09-20 01:01:00 +0100

    [HODUTU] - :factory: j2nedb path fixed

--- tools/json-to-nedb.js
+++ tools/json-to-nedb.js
@@ -1,5 +1,5 @@
 var Nedb = require('nedb');
-var userJsonData = require('/../src/trash-data/data.js');
+var userJsonData = require(__dirname + '/../src/trash-data/data.js');
 
 //var userList = new Nedb({ filename: 'src/data/user_data.db', autoload: true });
 
