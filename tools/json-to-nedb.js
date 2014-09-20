9ee6de1955f1756b01353f919f653d2f8404deed
Michał Budzyński <michal@virtualdesign.pl>
2014-09-20 01:01:00 +0100

    [HODUTU] - :grapes: proper json usage in json2nedb

--- tools/json-to-nedb.js
+++ tools/json-to-nedb.js
@@ -4,5 +4,5 @@ var userJsonData = require(__dirname + '/../src/trash-data/data.js');
 var userList = new Nedb({ filename: 'src/data/user_data.db', autoload: true });
 
 userJsonData.forEach(function(show){
-  console.log(show.name);
+  console.log(show.title);
 })
