fe0606da352bb38191c7c65d43411dfa87ae32e2
Michał Budzyński <michal@virtualdesign.pl>
2014-09-20 01:01:00 +0100

    [HODUTU] - :open_file_folder: lint fixes

--- src/js/theme_song.js
+++ src/js/theme_song.js
@@ -1,11 +1,14 @@
+'use strict';
+/* export themeSong */
+
 var themeSong = function(title, cb) {
   var request = require('request');
 
-  //var title = "Two And A Half Man";
-  var apiUrl = "http://www.youtube.com/results?search_query=";
+  //var title = 'Two And A Half Man';
+  var apiUrl = 'http://www.youtube.com/results?search_query=';
 
   console.log('elo!');
-  var url = apiUrl + title.replace(' ', '+') + "+theme+song";
+  var url = apiUrl + title.replace(' ', '+') + '+theme+song';
   console.log(url);
   request(url, function (error, response, body) {
     console.log('EEEEEE', error, response);
@@ -13,17 +16,19 @@ var themeSong = function(title, cb) {
       var firstIndex = body.indexOf('/watch?v=');
       var ytId = body.substr(firstIndex + 9, 25).split('"')[0];
       cb(
-        'http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=' +
+        'http://youtubeinmp3.com/fetch/?video=' +
+        'http://www.youtube.com/watch?v=' +
         ytId
       );
     }
   });
-}
+};
 
-
+module.exports = themeSong;
 
 // var getMp3FromYt = function(ytId) {
-//   var mp3Api = 'http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=';
+//   var mp3Api = 'http://youtubeinmp3.com/fetch/?vi
+//deo=http://www.youtube.com/watch?v=';
 //   request(mp3Api + ytId, function (error, response, body) {
 //     console.log(body);
 //
